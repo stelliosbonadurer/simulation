0b9a0b0a0af1f13a30e4e90b0c30ade263ea536b
User <user@example.com>
2026-02-20 19:47:22 +0000

    update Bullet Trajectory

--- funcs.py
+++ funcs.py
@@ -14,6 +14,19 @@ def EulerRichardson(dt, f, t, y, args): #Midpoint sampling
     y_mid = y + f(t, y, *args) * (dt / 2) #Gives us the velocity at the midpoint
     return y + f(t + dt/2, y_mid, *args) * dt #We calculate overall difference using midpoint velocity. y + velocity at midpoint * dt
 
+def OtherEuler(dt, f, t, y, args):
+    y_mid = y + f(t, y, *args) * (dt / 2) #Gives us the velocity at the midpoint
+    
+    return y + EulerRichardson(t + dt/2, y_mid, *args) * dt
+
+def RungeKutta4(dt,f,t,y,args):
+
+    h = dt
+    k1 = Euler(dt,f,t,y)
+    k2 = EulerRichardson(dt,f,t,y)
+    k3 = OtherEuler(dt,f,t,y)
+    
+
 
 def solve_ode(f, tspan, y0, method=EulerRichardson, *args, **options):
     """
@@ -55,3 +68,77 @@ def solve_ode(f, tspan, y0, method=EulerRichardson, *args, **options):
         
     return np.array(y), np.array(t)
 
+def gravitational(rij,i,j,p):
+    """
+    Using the dictionary of parameters p return the gravitational force between particles i
+    and j. We will assume that the vector between them has already been computed as rij.
+
+    Returns a vector force, having d components where d is the dimension of the problem.
+    
+    """
+    return rij * -p['G'] * p['m'][i] * p['m'][j] / (norm(rij)**3)
+    
+
+def n_body(t,y,p):
+    """
+    Develop the code that accepts a state vector y for N particles in d dimensions.
+    For example, in 3 dimensions:
+    y = [x1, y1, z1, x2, y2, z2, ... vx1, vy1, vz1, vx2, vy2, vz2,...]
+    Normally, this would be up to you, but for this assignment it
+    is better you follow my lead, because I have many interesting 
+    initial conditions (y0) for you to explore.
+
+    The function should then compute dydt for each of the input values.
+    It should do so using a force function that computes the magnitude of force.
+    That function should be a function handle in parameter dictionary p.
+
+    The force must be compute between every pair of particles i,j
+    This requires nested loops. 
+    Record the forces in a matrix. 
+    The sum of each row of the matrix corresponds to the total force on a particle.
+    The dictionary p will contain any and all additional parameters needed in the calculation.
+    """
+    masses = p['m']
+    N = len(masses)
+    d = p['dimension']
+    FMatrix = np.zeros((N,N,d))
+    midpoint = len(y)//2
+    
+    dydt = np.zeros(len(y))
+    dydt[0:midpoint] = y[midpoint:]     
+    
+    pos = np.array(y[:N*d]).reshape(N,d)
+    #print(pos)
+    #print(pos.shape)
+    
+    for i in range(N):
+        #Identify ith position vector
+        vi = pos[i]
+        
+        for j in range(i+1,N): 
+            #jth position vector
+            vj = pos[j]
+            rij = vi-vj
+        
+            fij = p['force'](rij,i,j,p)
+            
+            FMatrix[i,j] = fij
+            FMatrix[j,i] = -fij
+
+    #Sum forces on ith vector and map to dydt    
+    #Use sum to do this in one step not loop
+    for i in range(N):
+        '''i_dydt = np.sum(FMatrix[i],axis=0)
+        print(i_dydt)
+        print(i_dydt.shape)
+        print("dydt")
+        print(dydt)
+        print(dydt.shape)'''
+        
+        #set velocity half of to force taken from FMatrix, divide my masses to get force because F=ma -> a = F/m
+        dydt[midpoint + i*d:midpoint + (i+1)*d] = np.sum(FMatrix[i],axis=0)/masses[i]
+    
+    '''print(FMatrix)
+    print(FMatrix.shape)
+    print(f"dydt:{dydt}")'''
+    return dydt
